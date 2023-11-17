98360221744740a0c5b3d594d6f201bd44d3c663
Tech Gamer <148703509+gamerscodeuniverse@users.noreply.github.com>
2023-11-17 12:30:01 +0400

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from time import *
+
+
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B %d, %Y")
+    date_label.config(text=date_string)
+
+    window.after(1000, update)
+
+
+window = Tk()
+
+window.title("Clock Program")
+
+time_label = Label(window, font=("Arial", 50), fg="#00FF00", bg="black")
+time_label.pack()
+
+day_label = Label(window, font=("Ink Free", 50))
+day_label.pack()
+
+date_label = Label(window, font=("Ink Free", 50))
+date_label.pack()
+
+update()
+
+window.mainloop()
